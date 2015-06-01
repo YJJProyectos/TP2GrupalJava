e7c2816c807b8aa921165d17e3362696691826ae
Yang <YJJProyectos@users.noreply.github.com>
2015-06-01 20:35:08 -0300

    Cambios de mapa por HashMap<Coordenada,Ocupable> , y agrego de clase Coordenada

---
+++ src/tp2/Coordenada.java
@@ -0,0 +1,50 @@
+package tp2;
+
+public class Coordenada {
+	
+	private int fila;
+	
+	private int columna;
+	
+	public Coordenada(int fila,int columna){
+		
+		this.fila = fila;
+		this.columna = columna;
+	}
+	
+	public void setFila(int fila){
+		this.fila = fila; 
+	}
+	
+	public void setColumna(int columna){
+		this.columna = columna;
+	}
+	
+	public int getFila(){
+		return this.fila;
+	}
+	
+	public int getColumna(){
+		return this.columna;
+	}
+	
+	
+	@Override 
+	public int hashCode(){
+		
+		return (fila*fila + columna);
+	}
+	@Override
+	public boolean equals(Object otroObjeto){
+
+		if (otroObjeto.getClass() != this.getClass()){
+			return false;
+		}
+		else {
+			Coordenada otraCoordenada = (Coordenada) otroObjeto;
+			return ( otraCoordenada.getFila() == this.getFila() &&
+					otraCoordenada.getColumna() == this.getColumna() );
+		}
+	}
+
+}
--- src/tp2/Mapa.java
+++ src/tp2/Mapa.java
@@ -1,15 +1,34 @@
 package tp2;
 
+import java.util.HashMap;
+
 import tp2.Ocupable;
 
 public class Mapa {
-	Ocupable[][] tablero;
+	
+	HashMap<Coordenada,Ocupable> tablero = new HashMap<Coordenada,Ocupable>();
+	
+	// Ocupable[][] tablero;
 
 	public Mapa(int bases) {
-		tablero = new Ocupable[bases * 5][bases * 5];
+		
+		//tablero = new Ocupable[bases * 5][bases * 5];
+		Coordenada coordenada;
+		Ocupable ocupable;
+		for (int fila = 1; fila <= (bases * 5);fila++){
+			for (int columna = 1; columna <= (bases * 5); columna++){
+				
+			    coordenada = new Coordenada(fila,columna);
+			    ocupable = null;
+				tablero.put(coordenada,ocupable);
+			}
+			
+		}  
+
 	}
 
 	public int tamanio() {
-		return tablero.length * tablero[0].length;
+		//return tablero.length * tablero[0].length;
+		return tablero.size();
 	}
 }
