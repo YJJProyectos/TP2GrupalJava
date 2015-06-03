577615d4efeb65437806987dcb97d5db94a87f3d
Federico von Brudersdorff <fedevb94@hotmail.com>
2015-06-03 12:45:27 -0300

    Arreglo de redundancia de codigo entre estructura y unidad.

--- src/edificables/Estructura.java
+++ src/edificables/Estructura.java
@@ -2,37 +2,12 @@ package edificables;
 
 import interfaces.Edificable;
 import interfaces.Ocupable;
+import accionables.Unidad;
 
-public abstract class Estructura implements Edificable {
+public abstract class Estructura extends Unidad implements Edificable {
 
 	protected Ocupable posicion;
 	protected int vida;
-
-	@Override
-	public void posicionar(Ocupable casilla) {
-		if (casilla.ocupar(this))
-			this.posicion = casilla;
-	}
-
-	@Override
-	public void recibirDanio(int cantidadDanio) {
-		vida = vida - cantidadDanio;
-	}
-
-	@Override
-	public int vidaRestante() {
-		return vida;
-	}
-
-	@Override
-	public boolean estaDestruido() {
-		return (vida <= 0);
-	}
-
-	@Override
-	public Ocupable posicion() {
-		return posicion;
-	}
 
 	public abstract boolean edificar();
 
