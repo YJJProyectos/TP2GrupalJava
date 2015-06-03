8d3d9b79e7ee157500b26834e1b5133488b71271
santigandolfo <santigandolfo@users.noreply.github.com>
2015-06-03 00:41:11 -0300

    se cambio el nombre de casillaEspacial por casillaAerea

--- src/tp2/CasillaAerea.java
+++ src/tp2/CasillaAerea.java
@@ -3,7 +3,7 @@ package tp2;
 import tp2.Casilla;
 import interfaces.Accionable;
 
-public class CasillaEspacial extends Casilla {
+public class CasillaAerea extends Casilla {
 
 	public boolean esTerrestre() {
 		return false;
