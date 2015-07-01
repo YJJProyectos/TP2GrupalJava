d7097a86ba7462ebb0209a48c99f5c747d0f0df7
Yang <YJJProyectos@users.noreply.github.com>
2015-07-01 01:05:03 -0300

    borrado de partes comentadas

--- src/algo3/algocraft/vista/VentanaGanador.java
+++ src/algo3/algocraft/vista/VentanaGanador.java
@@ -10,26 +10,9 @@ public class VentanaGanador extends JFrame {
 
 	public VentanaGanador(Jugador ganador) {
 
-		JOptionPane.showMessageDialog(this, ganador.getNombre() + "ha ganado!",
+		JOptionPane.showMessageDialog(this, ganador.getNombre() + " ha ganado!",
 				"Ganador", JOptionPane.INFORMATION_MESSAGE, null);
 		System.exit(0);
 	}
 }
 
-// Object[] opciones = { "Continuar", "Salir" };
-
-// int n = JOptionPane.showOptionDialog(this, ganador.getNombre()
-// + "ha ganado!", "Ganador", JOptionPane.YES_NO_OPTION,
-// JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-
-// if (n == JOptionPane.YES_OPTION) {
-// try {
-// ventanaInicio.muestraPanelJuego();
-// } catch (NombresInvalidosError | CoordenadaInvalidaError error) {
-// String textoError = "<html>Nombres Invalidos <html>";
-// new VentanaError(textoError);
-// }
-// } else if (n == JOptionPane.NO_OPTION) {
-// System.exit(0);
-// }
-// }
